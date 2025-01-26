9d2acee46a330cbed4065f8076ce7abab370d90e
sffp.eu <contact@sffp.eu>
2025-01-26 19:37:18 +0100

    chore: cleanup duplicate configuration files - Remove redundant config files and keep the more complete versions

--- tailwind.config.js
+++
@@ -1,1 +0,0 @@
-module.exports = { darkMode: "class", content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"], theme: { extend: {} }, plugins: [] }
