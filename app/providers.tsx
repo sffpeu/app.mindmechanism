bb4dbb2fe276d25be9ed4ab870ab4f9435aa51c1
sffp.eu <contact@sffp.eu>
2025-01-27 22:59:50 +0100

    refactor: Remove Clerk authentication

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,14 +1,13 @@
 'use client'
 
-import { ClerkProvider } from '@clerk/nextjs'
-import { ThemeProvider } from './ThemeContext'
-
-export function Providers({ children }: { children: React.ReactNode }) {
+export function Providers({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <ClerkProvider>
-      <ThemeProvider>
-        {children}
-      </ThemeProvider>
-    </ClerkProvider>
+    <>
+      {children}
+    </>
   )
 } 
