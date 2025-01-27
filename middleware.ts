bac0eb1e9b62b364a10145b8fa72a6816a56f3cf
sffp.eu <contact@sffp.eu>
2025-01-27 20:25:32 +0100

    fix: simplify middleware using authMiddleware configuration

--- middleware.ts
+++ middleware.ts
@@ -1,42 +1,16 @@
-import { clerkMiddleware, getAuth } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { authMiddleware } from "@clerk/nextjs";
 
-const publicPaths = ["/", "/glossary", "/api/public(.*)", "/about"];
-const ignoredPaths = ["/api/webhooks(.*)"];
-
-function isPublic(path: string) {
-  return publicPaths.find((x) =>
-    path.match(new RegExp(`^${x}$`.replace("*$", "($|/)")))
-  );
-}
-
-function isIgnored(path: string) {
-  return ignoredPaths.find((x) =>
-    path.match(new RegExp(`^${x}$`.replace("*$", "($|/)")))
-  );
-}
-
-export default clerkMiddleware(async (req) => {
-  const { userId } = getAuth(req);
-  const path = req.nextUrl.pathname;
-  
-  if (isIgnored(path)) {
-    return NextResponse.next();
+export default authMiddleware({
+  publicRoutes: ["/", "/glossary", "/api/public(.*)", "/about"],
+  ignoredRoutes: ["/api/webhooks(.*)"],
+  afterAuth(auth, req) {
+    // Handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      const signInUrl = new URL('/sign-in', req.url);
+      signInUrl.searchParams.set('redirect_url', req.url);
+      return Response.redirect(signInUrl);
+    }
   }
-  
-  if (isPublic(path)) {
-    return NextResponse.next();
-  }
-  
-  // If the user is not signed in and the path is not public, redirect them to the sign-in page
-  if (!userId) {
-    const signInUrl = new URL('/sign-in', req.url);
-    signInUrl.searchParams.set('redirect_url', req.url);
-    return NextResponse.redirect(signInUrl);
-  }
-  
-  return NextResponse.next();
 });
 
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
