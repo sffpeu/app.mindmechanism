e4c6acd83e42e1c0732724da442f8f7eb6c58478
sffp.eu <contact@sffp.eu>
2025-01-27 20:17:18 +0100

    fix: simplify middleware matcher pattern

--- middleware.ts
+++ middleware.ts
@@ -17,9 +17,14 @@ export default clerkMiddleware({
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(html?|css|js|json|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - public (public files)
+     */
+    "/((?!_next/static|_next/image|favicon.ico|public).*)",
+    "/api/(.*)",
   ],
 }; 
